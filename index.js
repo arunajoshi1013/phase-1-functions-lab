3d9ad59fa44f794f556a33a2c479f1dceadd41b9
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-03-16 22:03:18 -0700

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(val) {
+   return Math.abs(val - 42)
+}
+
+function distanceFromHqInFeet(val) {
+   return (distanceFromHqInBlocks(val) * 264)
+}
+
+function distanceTravelledInFeet(s, d) {
+   return (Math.abs(d - s) * 264)
+}
+
+function calculatesFarePrice(s, d) {
+   const feet = distanceTravelledInFeet(s, d)
+
+   if (feet <= 400) {
+     return 0;
+   } else if (feet > 2500) {
+     return 'cannot travel that far'
+   } else if (feet < 2000) {
+     return ((feet - 400) * 2) / 100
+   } else {
+     return 25
+   }
+}
